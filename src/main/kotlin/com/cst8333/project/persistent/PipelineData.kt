b2f14e5ac33cd1224105f1e82d4a92e67deebba8
feiqiong-deng <feiqiong.deng@outlook.com>
2022-02-12 12:07:46 -0500

    add index.html

---
+++ src/main/kotlin/com/cst8333/project/persistent/PipelineData.kt
@@ -0,0 +1,9 @@
+package com.cst8333.project.persistent
+
+import com.cst8333.project.model.PipelineRecord
+
+class PipelineData : PipelineDataSource {
+    override fun getAll(): Collection<PipelineRecord> {
+        TODO("Not yet implemented")
+    }
+}
