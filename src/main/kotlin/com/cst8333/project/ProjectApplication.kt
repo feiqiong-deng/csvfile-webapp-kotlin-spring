ffe75f91f24dd13f14f81907b573ace75f12f172
feiqiong-deng <feiqiong.deng@outlook.com>
2022-02-25 20:52:30 -0500

    Modify some codes

--- src/main/kotlin/com/cst8333/project/ProjectApplication.kt
+++ src/main/kotlin/com/cst8333/project/ProjectApplication.kt
@@ -1,7 +1,11 @@
 package com.cst8333.project
 
+import com.cst8333.project.business.PipelineService
+import com.cst8333.project.persistent.PipelineData
+import com.cst8333.project.presentation.ProjectView
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.PathVariable
 
 @SpringBootApplication
 class ProjectApplication
@@ -13,4 +17,15 @@ class ProjectApplication
  */
 fun main(args: Array<String>) {
 	runApplication<ProjectApplication>(*args)
+	val source = PipelineData()
+	val service = PipelineService(source)
+	val view = ProjectView(service)
+	view.showAllRecords()
+	view.showSearchRecords("search")
+	view.addRecord()
+//	view.addOneRecord("a", "a", "a", "a","a", "a", "a", "a","a", "a", "a")
+	view.showEditRecords("search")
+	view.showDeleteRecords("search")
+	view.ediTOneRecord("a", "a", "a", "a","a", "a", "a", "a","a", "a", "a","a")
+	view.deleteRecords("search")
 }
