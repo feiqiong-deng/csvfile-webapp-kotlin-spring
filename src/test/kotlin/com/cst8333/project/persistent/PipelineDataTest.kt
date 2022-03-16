ce23bf15e88f34ebf958a52d4a670d5ae6ecfc27
feiqiong-deng <feiqiong.deng@outlook.com>
2022-03-16 14:23:51 -0400

    Add JUnit test to test newly added sort functionality

--- src/test/kotlin/com/cst8333/project/persistent/PipelineDataTest.kt
+++ src/test/kotlin/com/cst8333/project/persistent/PipelineDataTest.kt
@@ -5,6 +5,7 @@ import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+
 /**
  * This is the unit testing class using JUnit5 framework.
  * @author Feiqiong Deng
@@ -27,6 +28,7 @@ internal class PipelineDataTest {
     private val expected =  PipelineRecord("SN098","Release of Substance",
         "02/02/2022", "Test", "Ontario","ABC", "substance",
         "No", "Sweet")
+
 
     /**
      * This is the unit test to test adding one record method.
@@ -79,5 +81,111 @@ internal class PipelineDataTest {
         assertFalse(testData.getAllRecords().contains(searchRecord))
     }
 
+    /**
+     * This is the unit test to test sorting records method.
+     * The function firstly sort the records by each column, total 9 columns, one by one.
+     * Then the function retrieve all data of the sorted column to separate lists to be tested.
+     * The function retrieve all data of each column to separate lists before sorting.
+     * Each list of data for each column then is sorted by ascending order to be the lists of expected results.
+     * The function will then compare every element in each list of column after getSortedResults() with expected results.
+     */
+    @Test
+    fun testSortList() {
+        val sortByNumber = testData.getSortedResults("a")
+        val sortByType = testData.getSortedResults("b")
+        val sortByDate = testData.getSortedResults("c")
+        val sortByCenter = testData.getSortedResults("d")
+        val sortByProvince = testData.getSortedResults("e")
+        val sortByCompany = testData.getSortedResults("f")
+        val sortBySubstance = testData.getSortedResults("g")
+        val sortBySignificant = testData.getSortedResults("h")
+        val sortByCategory = testData.getSortedResults("i")
 
+        val listNumber = ArrayList<String>()
+        val listType = ArrayList<String>()
+        val listDate = ArrayList<String>()
+        val listCenter = ArrayList<String>()
+        val listProvince = ArrayList<String>()
+        val listCompany = ArrayList<String>()
+        val listSubstance = ArrayList<String>()
+        val listSignificant = ArrayList<String>()
+        val listCategory  = ArrayList<String>()
+
+        val expectedNumList = ArrayList<String>()
+        val expectedTypeList = ArrayList<String>()
+        val expectedDateList = ArrayList<String>()
+        val expectedCenterList = ArrayList<String>()
+        val expectedProvinceList = ArrayList<String>()
+        val expectedCompanyList = ArrayList<String>()
+        val expectedSubsList = ArrayList<String>()
+        val expectedSignificanceList = ArrayList<String>()
+        val expectedCategoryList = ArrayList<String>()
+
+        sortByNumber.forEach {
+            listNumber.add(it.number)
+        }
+        sortByType.forEach {
+            listType.add(it.type)
+        }
+        sortByDate.forEach {
+            listDate.add(it.date)
+        }
+        sortByCenter.forEach {
+            listCenter.add(it.centre)
+        }
+        sortByProvince.forEach {
+            listProvince.add(it.province)
+        }
+        sortByCompany.forEach {
+            listCompany.add(it.company)
+        }
+        sortBySubstance.forEach {
+            listSubstance.add(it.substance)
+        }
+        sortBySignificant.forEach {
+            listSignificant.add(it.significant)
+        }
+        sortByCategory.forEach {
+            listCategory.add(it.category)
+        }
+
+        testData.getAllRecords().forEach {
+            expectedNumList.add(it.number)
+        }
+        testData.getAllRecords().forEach {
+            expectedTypeList.add(it.type)
+        }
+        testData.getAllRecords().forEach {
+            expectedDateList.add(it.date)
+        }
+        testData.getAllRecords().forEach {
+            expectedCenterList.add(it.centre)
+        }
+        testData.getAllRecords().forEach {
+            expectedProvinceList.add(it.province)
+        }
+        testData.getAllRecords().forEach {
+            expectedCompanyList.add(it.company)
+        }
+        testData.getAllRecords().forEach {
+            expectedSubsList.add(it.substance)
+        }
+        testData.getAllRecords().forEach {
+            expectedSignificanceList.add(it.significant)
+        }
+        testData.getAllRecords().forEach {
+            expectedCategoryList.add(it.category)
+        }
+
+        assertThat(listNumber).isEqualTo(expectedNumList.sorted())
+        assertThat(listType).isEqualTo(expectedTypeList.sorted())
+        assertThat(listDate).isEqualTo(expectedDateList.sorted())
+        assertThat(listCenter).isEqualTo(expectedCenterList.sorted())
+        assertThat(listProvince).isEqualTo(expectedProvinceList.sorted())
+        assertThat(listCompany).isEqualTo(expectedCompanyList.sorted())
+        assertThat(listSubstance).isEqualTo(expectedSubsList.sorted())
+        assertThat(listSignificant).isEqualTo(expectedSignificanceList.sorted())
+        assertThat(listCategory).isEqualTo(expectedCategoryList.sorted())
+    }
 }
+
